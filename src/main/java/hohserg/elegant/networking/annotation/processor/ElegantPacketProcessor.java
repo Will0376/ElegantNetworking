171304ba99e87eaccce34a1d32eb907c33b02884
hohserg <hohserg1@yandex.ru>
2020-09-26 16:58:53 +0300

    Added simple processor

---
+++ src/main/java/hohserg/elegant/networking/annotation/processor/ElegantPacketProcessor.java
@@ -0,0 +1,64 @@
+package hohserg.elegant.networking.annotation.processor;
+
+import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableSet;
+import hohserg.elegant.networking.api.ElegantPacket;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+@AutoService(Processor.class)
+public class ElegantPacketProcessor extends AbstractProcessor {
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        typeUtils = processingEnv.getTypeUtils();
+        elementUtils = processingEnv.getElementUtils();
+        filer = processingEnv.getFiler();
+        messager = processingEnv.getMessager();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(ElegantPacket.class)) {
+            switch (annotatedElement.getKind()) {
+                case CLASS:
+                    note("found " + annotatedElement.getSimpleName());
+                    break;
+                default:
+                    error("@ElegantPacket can be applied only to classes");
+
+            }
+        }
+        return false;
+    }
+
+    private void note(String msg) {
+        messager.printMessage(Diagnostic.Kind.NOTE, msg);
+    }
+
+    private void error(String msg) {
+        messager.printMessage(Diagnostic.Kind.ERROR, msg);
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return ImmutableSet.of(ElegantPacket.class.getCanonicalName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.RELEASE_7;
+    }
+
+    private Types typeUtils;
+    private Elements elementUtils;
+    private Filer filer;
+    private Messager messager;
+}
